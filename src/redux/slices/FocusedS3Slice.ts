e0b8fecd6e8da3c1c4974da86e142810dd719b0f
ss35789 <ss35789@naver.com>
2022-09-30 18:27:45 +0900

    create FocusedS3Slice

---
+++ src/redux/slices/FocusedS3Slice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// Define a type for the slice state
+
+export const FocusedS3Slice = createSlice({
+  name: 'FocusedS3',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState: {
+    s3_id: -1
+  },
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    storeImage: (state, action: PayloadAction<number>) => {
+      state.s3_id = action.payload;
+    }
+  }
+});
+
+export const { storeFocusedS3 } = FocusedS3Slice.actions;
+
+export default FocusedS3Slice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { ThumbnailSlice } from './slices/ThumbnailSlice';
 import { ImageSlice } from './slices/ImageSlice';
 import { UserSlice } from './slices/UserSlice';
+import { FocusedS3Slice } from './slices/FocusedS3Slice';
 
 export const store = configureStore({
   reducer: {
     Thumbnail: ThumbnailSlice.reducer,
     Image: ImageSlice.reducer,
-    User: UserSlice.reducer
+    User: UserSlice.reducer,
+    FocusedS3: FocusedS3Slice.reducer
   }
 });
 
