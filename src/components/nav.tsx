5bed2519f01b38b880416ecdfbab8a677f251230
ryankimjh00 <ryankimjh546@gmail.com>
2022-11-23 14:25:49 +0900

    refactored design except profile component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import React, { useCallback, useEffect, useState } from 'react';
 import { LogOut } from '../features/Logout';
 import { AccessToken } from '../variable/token';
@@ -23,41 +23,32 @@ export const NavComponent = () => {
   return (
         <Navbar collapseOnSelect expand="lg" bg="black" variant="dark">
             <Container>
-                <Navbar.Brand href="/">ULS3</Navbar.Brand>
+                <Navbar.Brand href="/">URLS3</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/features">Features</Nav.Link>
-                        <Nav.Link href="/pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="/action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="/action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="/action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="/Information">Information</Nav.Link>
                     </Nav>
                     <Nav>
                         {loginStatus &&
-                            <Nav.Link href="/analytics">Analytics S3</Nav.Link>
+                            <Nav.Link href="/analytics">Analytics</Nav.Link>
                         }
                         {loginStatus &&
-                            <Nav.Link onClick={onClickToggleModal}>
-                                프로필
+                            // <Nav.Link onClick={onClickToggleModal}>
+                            //     Profile
+                            // </Nav.Link>
+                            <Nav.Link href="/profile">
+                                Profile
                             </Nav.Link>
                         }
                         {loginStatus &&
                             <Nav.Link onClick={LogOut}>
-                                로그 아웃
+                                LogOut
                             </Nav.Link>
                         }
                         {!loginStatus &&
                             <Nav.Link href="/signup">
-                                회원 가입
+                                SignIN
                             </Nav.Link>
                         }
 
