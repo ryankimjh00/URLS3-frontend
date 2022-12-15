5b756aef2ab634f59a2e16013457ce68b7e528c5
b22016645 <byeolstar12@naver.com>
2022-12-15 16:16:36 +0900

    다국어 추가중

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import "./locales";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
